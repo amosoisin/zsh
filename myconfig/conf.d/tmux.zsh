db4cd7d40c2c3662366a1319764a5e47d599eeaf
amosoisin <23148082+amosoisin@users.noreply.github.com>
2025-12-31 08:21:07 +0900

    ペインのレイアウトのaliasを変更

--- myconfig/conf.d/tmux.zsh
+++ myconfig/conf.d/tmux.zsh
@@ -1,16 +1,31 @@
-tmux_layout() {
+vh_layout() {
     tmux split-window -v
     tmux split-window -h
-    tmux resize-pane -D 15
+    tmux resize-pane -D 20
     tmux select-pane -t 0
 }
+
+v_layout() {
+    tmux split-window -v
+    tmux resize-pane -D 20
+    tmux select-pane -t 0
+}
+
+h_layout() {
+    tmux split-window -h
+    tmux resize-pane -R 100
+    tmux select-pane -t 0
+}
+
 
 tmux_config() {
     if [ -z "$TMUX" ] && [ -t 1 ]; then
         tmux new -A -s dev
     fi
 
-    alias ppp=tmux_layout
+    alias tvh=vh_layout
+    alias tv=v_layout
+    alias th=h_layout
 }
 
 tmux_config
